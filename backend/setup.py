dc6b721558ee1ee3cec65d5b73d5376247324818
haeric <hakon.erichsen@gmail.com>
2011-10-29 11:48:00 +0200

    Setting up buildout, splitting in frontend and backend

---
+++ backend/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "DigipostSync", 
+    version = "1.0",
+    url = 'http://github.com/haeric/digipost',
+    license = 'Proprietary',
+    description = "Connecting Digipost to Dropbox, Gmail, Facebook and more",
+    author = 'Haakon Erichsen',
+    requires = [],
+    install_requires = ['setuptools'],
+)
+
+
